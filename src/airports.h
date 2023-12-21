83d4fad33d867ae962b5f0e0a2b517c01d687961
Viktoriia1722 <viktoriakutsel@gmail.com>
2023-12-21 18:26:50 +0100

    airopots

---
+++ src/airports.h
@@ -0,0 +1,24 @@
+//
+// Created by vikto on 12/21/2023.
+//
+
+#ifndef AIRTRVL_AIRPORTS_H
+#define AIRTRVL_AIRPORTS_H
+
+#include<string>
+#include<vector>
+
+struct Airport {
+    std::string code;
+    std::string name;
+    std::string city;
+    std::string country;
+    double latitude;
+    double longitude;
+};
+class AirportDataReader {
+public:
+    std::vector<Airport> readAirportsFromFile(const std::string& filepath);
+};
+
+#endif //AIRTRVL_AIRPORTS_H
